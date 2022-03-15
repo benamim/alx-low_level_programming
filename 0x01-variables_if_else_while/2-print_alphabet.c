59db3de7bf0874d425309c27fb1fdcacccfb5eba
benamim <98756752+benamim@users.noreply.github.com>
2022-03-16 01:28:29 +0300

    ebo

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+puchar (alphabet);
+}
+
+putchar('\n);
+
+return (0);
+}
