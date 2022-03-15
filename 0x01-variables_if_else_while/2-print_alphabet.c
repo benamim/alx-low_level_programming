7c0ae0dec3d9091c49a8efec56c06155a27d6728
benamim <98756752+benamim@users.noreply.github.com>
2022-03-16 01:39:49 +0300

    ebo

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,7 +3,7 @@
  * main - Entry point
  *
  * Return: Always 0 (success/correct)
-*/
+ */
 int main(void)
 
 {
@@ -11,10 +11,11 @@ char alphabet;
 
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
-putchar (alphabet);
+putchar(alphabet);
 }
 
-putchar('\n);
+putchar('\n');
 
 return (0);
+
 }
