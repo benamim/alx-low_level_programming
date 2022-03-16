69e27708ffb53b930ed2a8c488c2b8bd841eff83
benamim <98756752+benamim@users.noreply.github.com>
2022-03-16 21:49:13 +0300

    jack

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabetic character
+ * @c: takes in character
+ * Return: 1 if letter, lowercase, uppercase; otherwise 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'A' && C <= 'Z') || (c >= 'a' && c <= 'Z'))
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+}
