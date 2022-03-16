ee0d75a30f5115ff79d390106543ace82761fcf1
benamim <98756752+benamim@users.noreply.github.com>
2022-03-16 23:06:29 +0300

    hot

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,28 +2,32 @@
 /**
  * print_sign - checks for a
  * @c: takes in an integer
- * return: 1 if positive, -1 if negative, /if not a digit.
+ * Return: 1 if positive, -1 if negative, /if not a digit.
  */
-int print_sign (int n)
+int print_sign(int n)
 {
+
 if (n > 0)
 {
 _putchar('+');
 return (1);
 }
+
 else if (n == 0)
 {
 	_putchar('0');
 	return (0);
 }
+
 else if (n < 0)
 {
 _putchar('-');
 return (-1);
 }
+
 else
 {
 _putchar('-');
-return ('/');
+return ('-1');
 }
 }
