b26937fc0b93a47263ff24df358c726a99248bc6
benamim <98756752+benamim@users.noreply.github.com>
2022-03-16 22:45:42 +0300

    hot

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,7 +6,7 @@
  */
 int _print_sign(int n)
 {
-if (n>0)
+if (n > 0)
 {
 _putchar('+');
 return (1);
@@ -16,7 +16,7 @@ else if (n == 0)
 	_putchar('0');
 	return (-1);
 }
-else if
+else if (n < 0)
 {
 _putchar('-')
 return (-1);
