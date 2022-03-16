045b193333cea22601beeea8773f7a39c2dd72e8
benamim <98756752+benamim@users.noreply.github.com>
2022-03-17 00:41:52 +0300

    haiger

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,7 +9,6 @@
 * Return: void
 */
 
-
 void print_remaining_days(int month, int day, int year)
 {
 if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
@@ -18,8 +17,7 @@ if (month >= 3 && day >= 60)
 {
 day++;
 }
-
-
+{
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
@@ -35,6 +33,6 @@ printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
 }
-}
+
 
 
