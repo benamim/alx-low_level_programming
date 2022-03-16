bf62a14b6666ab3fbaebe627dc4cc070bd7f4060
benamim <98756752+benamim@users.noreply.github.com>
2022-03-16 22:40:29 +0300

    hot

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,10 +4,25 @@
  * @c: takes in an integer
  * return: 1 if positive, -1 if negative, / otherwise 0
  */
-int _abs(int n)
+int _print_sign(int n)
 {
-	if (n>0)
+if (n>0)
 {
-	return (n * -1);
-return (n);
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+	_putchar('0');
+	return (-1);
+}
+else if
+{
+_putchar('-')
+return (-1);
+}
+else
+{
+_putchar('-');
+return ('/');
 }
