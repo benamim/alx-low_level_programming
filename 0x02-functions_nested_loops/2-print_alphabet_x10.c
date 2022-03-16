8069471479ece2ecc8da43f9ebfe3d7cc697f2a6
benamim <98756752+benamim@users.noreply.github.com>
2022-03-16 21:31:48 +0300

    benas

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_alphabet_x10 - prints the alphabet in lower case 10 times
  *
- * Return: x10 a-z
+ * Return: Always 0.
  */
 void print_alphabet_x10(void)
 {
@@ -12,7 +12,7 @@ char c;
 
 for (i = 0; i < 10; i++)
 {
-for (c = 'a'; c <= 'Z'; c++)
+for (c = 'a'; c <= 'z'; c++)
 _putchar(c);
 _putchar('\n');
 }
