4ac2053a5b608950b51bef1bd3090b76e429ff31
benamim <98756752+benamim@users.noreply.github.com>
2022-03-31 10:08:14 +0300

    bosso

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number.
+ * @n: input number.
+ * Return: factorial of the number.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+	else
+		return (n * factorial(n - 1));
+}
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+3-factorial.c [unix] (10:06 31/03/2022)                                0,0-1 All
+"3-factorial.c" 0L, 0B
+
